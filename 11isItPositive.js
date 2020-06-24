0f7a61f70e1a7806813100d0bc3974b05ac64177
Diana <DianaMaer@users.noreply.github.com>
2020-06-24 16:37:32 +0200

    solve ex11 and ex12

--- 11isItPositive.js
+++ 11isItPositive.js
@@ -1,3 +1,16 @@
-// create the function isItPositive that will receive a number as argument and return:
-// 1 if the number is positive, 0 if the number received is zero and -1 if the number received is negative
+// create the function isItPositive that will 
+//receive a number as argument and return:
+// 1 if the number is positive, 0 if the number received 
+//is zero and -1 if the number received is negative
 
+const isItPositive = (number) => {
+    if (number > 0) { 
+        return 1
+    } else if (number < 0) {
+        return -1
+    } else return 0;
+}
+
+console.log(isItPositive(3));
+console.log(isItPositive(0));
+console.log(isItPositive(-6));
--- 12luckyNumber.js
+++ 12luckyNumber.js
@@ -1,4 +1,17 @@
-// create a function luckyNumber that will receive a number as a parameter and return if it is a lucky number or not. A lucky number must match these conditions:
+// create a function luckyNumber that will receive 
+//a number as a parameter and return if it is a lucky number or not. 
+//A lucky number must match these conditions:
 // it is positive
 // it is a multiple of 2 or 3
 // it is not 15
+
+const luckyNumber = (num) => {
+    if (num > 0 && (num%2 === 0 || num%3 === 0) && (num !== 15)) {
+        return `${num} is lucky`
+    } else return  `${num} is  not lucky`
+}
+
+
+console.log(luckyNumber(7));
+console.log(luckyNumber(8));
+console.log(luckyNumber(15));
