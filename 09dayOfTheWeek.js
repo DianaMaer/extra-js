f66170c4b70dd7fa88bc6afe3d20e3ae3cf16944
Diana <DianaMaer@users.noreply.github.com>
2020-06-24 16:26:28 +0200

    solve ex9 and ex10

--- 09dayOfTheWeek.js
+++ 09dayOfTheWeek.js
@@ -1,1 +1,12 @@
-// create a function dayOfTheWeek that will receive a string of a day of the week as a parameter, and return 'take some rest!' if the argument received is 'Sunday', otherwise, return 'no time to rest, it's just ...' where ... is the day of the week
+// create a function dayOfTheWeek that will 
+//receive a string of a day of the week as a parameter, 
+//and return 'take some rest!' if the argument received is 'Sunday', 
+//otherwise, return 'no time to rest, it's just ...' where ... is the 
+//day of the week
+
+const dayOfTheWeek = (day) => {
+    return day === 'Sunnday' ? 'take some rest': `no time to rest, it's just ${day}`;
+
+}
+
+console.log(dayOfTheWeek('Monday'));
--- 10largerThan.js
+++ 10largerThan.js
@@ -1,1 +1,17 @@
-// create a function largerThan that will receive 2 numbers as parameters and return the largest number only
+// create a function largerThan that will receive 
+//2 numbers as parameters and return the largest number only
+
+const largerThan = (num1, num2) => {
+    if (num1 > num2) {
+        return num1
+    } else {
+      if (num2 > num1) {
+          return num2
+        }
+    else return `Equal numbers`;
+}
+}
+
+console.log(largerThan(2,3));
+console.log(largerThan(5,3));
+console.log(largerThan(3,3));
